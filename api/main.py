e70d433b0e2ae7d92ada7c552bccf28f0a3c16d5
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-03-11 20:57:33 +0100

    feat: Update main.py

--- api/main.py
+++ api/main.py
@@ -58,7 +58,3 @@ async def invoice(order_id: str, _=Security(get_api_key)):
 @app.get("/")
 def root(_=Security(get_api_key)):
     return {"message": "Welcome to the RACIUS CARE INVOICE API!"}
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
